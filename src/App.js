210e82ced36b40f2ff996b1613c73af1156fb18e
Zack Eisbach <zack.eisbach@gmail.com>
2023-02-26 12:50:35 -0500

    add api call, menu rendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import "./App.css";
+import { useEffect, useState } from "react";
 import { MenuItem } from "./components/MenuItem";
 
 function App() {
@@ -172,7 +174,49 @@ function App() {
     calories: 200,
   };
 
-  return <MenuItem item={testItem} />;
+  const [menuSteast, setMenuSteast] = useState();
+
+  async function getMenuSteast() {
+    await axios
+      .get(
+        "https://api.dineoncampus.com/v1/location/586d05e4ee596f6e6c04b527/periods?platform=0&date=2023-2-26"
+      )
+      .then(({ data }) => {
+        if (data.error) {
+          console.error(data.error);
+          alert("An error occurred.");
+        } else {
+          setMenuSteast(data);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        alert("An error occurred.");
+      });
+  }
+
+  useEffect(() => {
+    getMenuSteast();
+  }, []);
+
+  // Steast:
+  // https://api.dineoncampus.com/v1/location/586d05e4ee596f6e6c04b527/periods?platform=0&date=YYYY-MM-DD
+
+  // IV:
+  // https://api.dineoncampus.com/v1/location/5f4f8a425e42ad17329be131/periods?platform=0&date=YYYY-MM-DD
+
+  // return <MenuItem item={testItem} />;
+  return (
+    <div>
+      {menuSteast ? (
+        menuSteast.menu.periods.categories.map((station) =>
+          station.items.map((item) => <MenuItem item={item} />)
+        )
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
 
 export default App;
