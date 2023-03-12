74e1ea620cf561fe426fec50e35f95555b31cf8d
Zack Eisbach <zack.eisbach@gmail.com>
2023-03-12 13:10:48 -0400

    add numeric values

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -7,15 +7,15 @@ export function MenuItem({ item }) {
   return (
     <div>
       <h1>{item.name}</h1>
-      <h2>{item.nutrients[0].value} Calories</h2>
+      <h2>{item.nutrients[0].value_numeric} Calories</h2>
       <ItemBadges filters={item.filters} />
       <button onClick={() => setDropdown(!dropdown)}>▼</button>
       {dropdown && (
         <div>
-          <p>{item.nutrients[1].value}g Protein</p>
-          <p>{item.nutrients[2].value} Total Carbohydrates</p>
-          <p>{item.nutrients[3].value}g Sugar</p>
-          <p>{item.nutrients[4].value}g Total Fat</p>
+          <p>{item.nutrients[1].value_numeric}g Protein</p>
+          <p>{item.nutrients[2].value_numeric}g Carbohydrates</p>
+          <p>{item.nutrients[3].value_numeric}g Sugar</p>
+          <p>{item.nutrients[4].value_numeric}g Total Fat</p>
           <p>Ingredients: {item.ingredients}</p>
         </div>
       )}
